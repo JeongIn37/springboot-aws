5857f3cc988c5b6ca58e687592e694f689c5cabb
yoonjeongin <yoonjeongin@naver.com>
2022-03-16 23:39:29 +0900

    [Chapter03] JPA: Gradle Setting

---
+++ src/main/java/example/org/springboot/domain/posts/Posts.java
@@ -0,0 +1,11 @@
+package example.org.springboot.domain.posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+public class Posts {
+
+}
